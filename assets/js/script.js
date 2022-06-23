0d959cfdbcbdced663482982306c26e82a4f6c1d
Ruminert <ruminertt@tanners-air.attlocal.net>
2022-06-22 22:09:18 -0500

    dynamic questions

--- assets/js/script.js
+++ assets/js/script.js
@@ -2,42 +2,49 @@
 var startButton = document.querySelector("#start-button");
  var questionDisplay = document.querySelector("#main-header");
  var answerDisplayEl = document.querySelector("#answers");
- var nextButton = document.querySelector('next-btn');
- var questionNum = 0;
+ var nextButton = document.querySelector("#next-btn");
+ var playerScore = 0;
+ var questionNum =  0;
+ var mainContent = document.querySelector('.main-content')
+ 
  
  var questionObject = [
    
  {   question: "what is a Boolean?",
-     answers: [{text: "true", correct: false },
-     {text: "false", correct: false },
-     {text: "both", correct: true }],
+     answers: [{text: "true", correct: false, id:0},
+     {text: "false", correct: false ,id:0},
+     {text: "both", correct: true , id:25},
+     {text: "none", correct: false, id:0}],
      
  },
 
  {   question: "What does HTML stand for?",
-     answers: [{text:"Hypertext Markup Language", correct: true}, 
-     {text:"Happy Times Meeting Lobbyists", correct: false}, 
-     {text:"Hens Train Male Llamas", correct: false},
-     {text:"Hyper Test Markdown Language", correct: false}],
+     answers: [{text:"Hypertext Markup Language", correct: true, id:25}, 
+     {text:"Happy Times Meeting Lobbyists", correct: false, id:0}, 
+     {text:"Hens Train Male Llamas", correct: false, id:0},
+     {text:"Hyper Test Markdown Language", correct: false, id:0}],
 },
 {   question: "Which answer is the 'not' operator",
-    answers: [{text:"null", correct: false}, 
-            {text:"&", correct: false}, 
-            {text:"Na", correct: false}, 
-            {text:"!", correct: true}],
+    answers: [{text:"null", correct: false, id:0}, 
+            {text:"&", correct: false, id:0}, 
+            {text:"Na", correct: false, id: 0}, 
+            {text:"!", correct: true, id: 25}],
 },
 
 {   question: "Which one is not a primitive data-type in javascript",
-    answers: [{text:"string", correct: false},
-            {text:"float", correct: true}, 
-            {text:"boolean", correct: false}, 
-            {text:"number", correct: false}],
+    answers: [{text:"string", correct: false, id:0},
+            {text:"float", correct: true, id:25}, 
+            {text:"boolean", correct: false, id:0}, 
+            {text:"number", correct: false, id:0}],
 }
 ];
 
 
  var startQuiz = function() {
-     nextQuestion();
+    //startButton.classList.add('hide');
+     
+   // mainContent.classList.remove('hide');
+    nextQuestion();
      
 // //var quizQuestionEl = document.createElement("div");
 // //quizQuestionEl.className = "questions";
@@ -68,7 +75,7 @@ var startButton = document.querySelector("#start-button");
  }
 
 var nextQuestion = function() {
-    reset()
+    reset();
     show(questionObject[questionNum]);
     
 }
@@ -77,10 +84,15 @@ var show = function(question) {
     questionDisplay.innerText = question.question;
     question.answers.forEach(answers => {
         var answerButton = document.createElement('button');
-        answerButton.innerHTML = answers.text;
+        answerButton.innerText = answers.text;
+        answerButton.classList.add('btn')
         if(answers.correct) {
             answerButton.dataset.correct = answers.correct;
+            
         }
+       
+
+        
         answerButton.classList.add('button');
         
         answerButton.addEventListener("click", select);
@@ -94,17 +106,53 @@ var show = function(question) {
 
 
 
-var select = function(e){
+var select = function(event){
+    var targetedButton = event.target;
+    var correct = targetedButton.dataset.correct;
+    setStatusClass(document.body, correct)
+    
+    Array.from(answerDisplayEl.children).forEach(button => {
+        setStatusClass(button, button.dataset.correct)
+    
+
+    })
+
+
+
+
+   
+   
+    // if (targetedButton) {
+    // playerScore += 25;
+    // console.log(playerScore)
+    // }
+    
+    
+    // playerScore += targetedButton;
+    // console.log(targetedButton);
 
 }
 
 var reset = function(){
-    
+    clearStatusClass(document.body)
     while (answerDisplayEl.firstChild) {
         answerDisplayEl.removeChild(answerDisplayEl.firstChild);
     }
 
 };
+function setStatusClass(element, correct) {
+    clearStatusClass(element)
+    if (correct) {
+      element.classList.add('correct')
+    } else {
+      element.classList.add('wrong')
+    }
+  }
+  
+  function clearStatusClass(element) {
+    element.classList.remove('correct')
+    element.classList.remove('wrong')
+  }
 
 
 
@@ -119,4 +167,8 @@ var reset = function(){
 // }
 
 startButton.addEventListener("click", startQuiz);
+nextButton.addEventListener('click', () => {
+    questionNum++
+    nextQuestion()
+  });
 
